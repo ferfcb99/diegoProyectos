e19a2e5b30b52dd6bc9aa130a22fac630852b81e
ferfcb99 <fer_fcb99@outlook.com>
2023-07-31 20:53:35 -0600

    Se crean funciones para cajero

--- src/cajero/Operaciones.java
+++ src/cajero/Operaciones.java
@@ -1,12 +1,14 @@
 package cajero;
 
+import java.util.Scanner;
+
 public class Operaciones {
-    
-    public Operaciones(){
+
+    public Operaciones() {
         System.out.println("Ejecutando constructor");
     }
-    
-    public Operaciones(int montoTotal, String nombre, String apellido, String noCuenta, String nip, String fechaVigencia){
+
+    public Operaciones(int montoTotal, String nombre, String apellido, String noCuenta, String nip, String fechaVigencia) {
         this.montoTotal = montoTotal;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -25,7 +27,8 @@ public class Operaciones {
     public String noCuenta = "";
     public String nip = "";
     public String fechaVigencia = "";
-    
+    Scanner scanner;
+
     // getters : obtener y setters : colocar
     public int getMontoTotal() {
         return this.montoTotal;
@@ -70,15 +73,65 @@ public class Operaciones {
     public void setFechaVigente(String fechaVigencia) {
         this.fechaVigencia = fechaVigencia;
     }
-    
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaVigencia(String fechaVigencia) {
+        this.fechaVigencia = fechaVigencia;
+    }
+
+    public String getFechaVigencia() {
+        return fechaVigencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Operaciones{" + "montoTotal=" + montoTotal + ", nombre=" + nombre + ", apellido=" + apellido + ", noCuenta=" + noCuenta + ", nip=" + nip + ", fechaVigencia=" + fechaVigencia + '}';
+    }
+
     // crear una funcion que agregue al montoTotal una cantidad con el nombre de cantidad
-    
-   public void agregueMonto(int cantidad){
-    this.montoTotal +=cantidad;
-   }
+    public void agregueMonto(int cantidad) {
+        this.montoTotal += cantidad;
+    }
+
     // crear una funcion que quite al monto total una cantidada con el nomrbe de cantidad
-   public void quitaMonto(int cantidad){
-       this.montoTotal -= cantidad;
-   }
+    public void quitaMonto(int cantidad) {
+        this.montoTotal -= cantidad;
+    }
+
+    public void muestraDatos() {
+        System.out.println(this.toString());
+    }
+
+    public void modificaDatos() {
+        String nombre = "";
+        String apellido = "";
+        String nip = "";
+        scanner = new Scanner(System.in);
+        System.out.println("Ingresa el nuevo nombre: ");
+        nombre = scanner.nextLine();
+        System.out.println("Ingresa el nuevo apellido: ");
+        apellido = scanner.nextLine();
+        System.out.println("Ingresa el nuevo nip: ");
+        nip = scanner.nextLine();
+
+        if (!(nombre.equals("") && apellido.equals("") && nip.equals(""))) {
+            this.setNombre(nombre);
+            this.setApellido(apellido);
+            this.setNip(nip);
+            System.out.println("Datos modificados exitosamente!!");
+        } else {
+            System.out.println("Ingreso datos nulos, regresando al menu...");
+        }
+    }
+
+    public void deposito() {
+        scanner = new Scanner(System.in);
+        System.out.println("Cantidad a depositar: ");
+        int monto = scanner.nextInt();
+        this.agregueMonto(monto);
+    }
 
 }
--- src/cajero/Principal.java
+++ src/cajero/Principal.java
@@ -1,18 +1,57 @@
-
 package cajero;
 
+import java.util.Scanner;
+
 public class Principal {
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion = -1;
+
         Operaciones comunicador = new Operaciones(10000,
-                "Diego", "Jimenez", "12342536472826623", "456","20230712"
+                "Diego", "Jimenez", "12342536472826623", "456", "20230712"
         );
-        
+        /* 
         System.out.println(comunicador.getNombre());
- System.out.println("Nombre del titular: " + comunicador.getNombre());
+    System.out.println("Nombre del titular: " + comunicador.getNombre());
     System.out.println("Monto total antes de agregar: " + comunicador.getMontoTotal());
     comunicador.agregueMonto(500);
     System.out.println("Monto total después de agregar 500: " + comunicador.getMontoTotal());
-      
-        
+    comunicador.quitaMonto(5000);
+        System.out.println(comunicador.getMontoTotal());
+         */
+        while (opcion != 0) {
+            System.out.println("------MENU-----");
+            System.out.println("1. Consulta de datos");
+            System.out.println("2. Modificacion de datos");
+            System.out.println("3. Deposito");
+            System.out.println("4. Retiro");
+            System.out.println("0. Salir");
+
+            System.out.println("Opcion a elegir: ");
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("----------MOSTRANDO DATOS-----------");
+                comunicador.muestraDatos();
+            } else if (opcion == 2) {
+                comunicador.modificaDatos();
+
+            } else if (opcion == 3) {
+                comunicador.deposito();
+
+            } else if (opcion == 4) {
+                System.out.println("Cuatro");
+
+            } else if (opcion == 0) {
+                break;
+            } else {
+                System.out.println("Debes elegir una de las opciones del menu mostrado");
+                continue;
+            }
+
+        }
+
     }
 }
