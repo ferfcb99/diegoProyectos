962dabcd16ab70dcc0e6008cd0d8a372492dac45
Diego Lopez <die_elias2012@hotmail.com>
2023-08-30 20:01:11 -0600

    Factorial

--- src/recursividad/Factorial.java
+++ src/recursividad/Factorial.java
@@ -1,12 +1,19 @@
 package recursividad;
 
 public class Factorial {
+
     // realizar un algortmo recursivo que calcule el factorial de un numero
     // 5! = 5x4x3x2x1 = 1x2x3x4x5
-     public static int Factorial(int n) {
-        
-        return Factorial(n-1);
+    public static int Factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * Factorial(n - 1);
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(Factorial(5));
     }
 
-    
 }
