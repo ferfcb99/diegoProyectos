9aee1172d66345ef1140b6c42a51289f2ffb0248
ferfcb99 <fer_fcb99@outlook.com>
2023-09-11 21:06:24 -0600

    Se complementa ejercicio de la raiz digital y se comienza ejercicio de numeros malvados

---
+++ src/recursividad/NumerosMalvados.java
@@ -0,0 +1,14 @@
+
+package recursividad;
+
+public class NumerosMalvados {
+    
+    // funcion convierta un numero a binario -> convierteABinario(5) -> 101
+    
+    
+    // funcion que cuente cuantos digitos k hay en un numero m -> cantidadDigitosEnNumero(21522, 2) -> 3
+    
+    
+    
+    
+}
--- src/recursividad/RaizDigitalRecursiva.java
+++ src/recursividad/RaizDigitalRecursiva.java
@@ -1,30 +1,86 @@
-
 package recursividad;
 
 public class RaizDigitalRecursiva {
-    public static int raizDigital(int numero)
-{
-    if (numero < 10)
-    {
-        return numero;
-    }
-    else
-    {
-        int sumaDigitos = sumaDeDigitos(numero);
-        return raizDigital(sumaDigitos);
-    }
-}
 
-private static int sumaDeDigitos(int numero)
-{
-    if (numero == 0)
-    {
-        return 0;
+    public static int raizDigital(int numero) {
+        if (numero < 10) {
+            return numero;
+        } else {
+            int sumaDigitos = sumaDeDigitos(numero);
+            return raizDigital(sumaDigitos);
+        }
     }
-    else
-    {
-        return numero % 10 + sumaDeDigitos(numero / 10);
+
+    private static int sumaDeDigitos(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else {
+            return numero % 10 + sumaDeDigitos(numero / 10);
+        }
     }
-}
+
+    // funcion que muestra las raices en un intervalo [x, y]
+    public void muestraRaices(int inicio, int fin) {
+        for (int i = inicio; i < fin; i++) {
+            System.out.println(i + " - " + raizDigital(i));
+        }
+    }
+
+    public int cuentaRaicesXEnIntervalo(int raizAContar, int inicio, int fin) { // cuentaRaicesXEnIntervalo(2, 10, 100) = 10
+                                                                                // cuentaRaicesXEnIntervalo(3, 10, 100) = 10
+        int cuentaRaices = 0;
+        for (int i = inicio; i < fin; i++) {
+            if (raizDigital(i) == raizAContar) {
+                cuentaRaices = cuentaRaices + 1;
+            }
+        }
+        return cuentaRaices;
+    }
     
+    public boolean hayIgualCantidadDeRaicesEnIntervalo(int inicio, int fin){    // hayIgualCantidadDeRaicesEnIntervalo(10, 100) : true, si hay igual cantidad 1-9
+        boolean existenIgualCantidad = true;
+        int totalRaices = cuentaRaicesXEnIntervalo(1, inicio, fin); // 10
+        for (int i = 2; i < 10; i++) { // 2,3,4,5,6,7,8,9
+            int raicesTemporales  = cuentaRaicesXEnIntervalo(i, inicio, fin); // 10 10 10 10 10
+            if(totalRaices != raicesTemporales){
+                existenIgualCantidad = false;
+                break;
+            }
+        }
+        return existenIgualCantidad;
+    }
+    
+    public void muestraSolucion(int cantidadDeIntervalos){ // 3 -> 1, 10 | 10, 100 | 100, 1000
+        int inicioIntervalo = 1;
+        int finIntevalo = 10;
+        boolean hayIgualCantidad = false;
+        for (int i = cantidadDeIntervalos; i > 0; i--) {
+            hayIgualCantidad = hayIgualCantidadDeRaicesEnIntervalo(inicioIntervalo, finIntevalo); // 1, 10
+            if(hayIgualCantidad == true){
+                System.out.println("En el intervalo [" + inicioIntervalo + ", " + (finIntevalo - 1) + "] hay igual cantidad de raices");
+            }else{
+                System.out.println("En el intervalo [" + inicioIntervalo + ", " + (finIntevalo - 1) + "] NO hay igual cantidad de raices");
+            }
+            inicioIntervalo = inicioIntervalo * 10;
+            finIntevalo = finIntevalo * 10;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        RaizDigitalRecursiva raiz = new RaizDigitalRecursiva();
+        // System.out.println(raizDigital(123456)); // 21 -> 3
+        // raiz.muestraRaices(100, 1000);
+       /* System.out.println("Raiz 5");
+        System.out.println(raiz.cuentaRaicesXEnIntervalo(5, 10, 100));
+        System.out.println("Raiz 7");
+        System.out.println(raiz.cuentaRaicesXEnIntervalo(7, 10, 100));
+        System.out.println("Raiz 3");
+        System.out.println(raiz.cuentaRaicesXEnIntervalo(3, 10, 100));
+    */
+       
+        //System.out.println(raiz.hayIgualCantidadDeRaicesEnIntervalo(10, 100));
+        raiz.muestraSolucion(8);
+    }
+
 }
